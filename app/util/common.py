70393ccbd27204953bc735e5f6b3d504f6fcd8e8
panhzh3 <haizhipan@gmail.com>
2015-03-06 09:58:54 +0800

    update util module

--- app/util/common.py
+++ app/util/common.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-from flask import jsonify
+from flask import jsonify, request
 
 def jsonError(ERROR):
     return jsonify({'code': ERROR[0], 'data': ERROR[1]})
 
 def jsonResponse(data):
     return jsonify({'code': 0, 'data': data})
+
+def viaMobile():
+    s = request.headers.get('User-Agent')
+    kw = ['micromessenger', 'Mobile', 'iPhone', 'Windows Phone', 'UCWEB', 'Fennec', 'Opera Mobi', 'BlackBerry', ]
+    for i in kw:
+        if i in s:
+            return True
+    return False
+
