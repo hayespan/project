fedfc69be6cec28e4708c0f3f463bc91975c9f9d
Yiqi <571738739@qq.com>
2015-03-22 14:17:18 +0800

    ...

--- app/static/admin/js/index_1.js
+++ app/static/admin/js/index_1.js
@@ -1037,9 +1037,7 @@ function createProduct(f) {
 	var price = f.word[1].value;
 	var cat2_id = $("#cat2Select").find('option:selected').attr('class');
 	var url = "/admin/level1/product/create";
-    var img = $(f).find("#inputfile").val();
-  	var formdata=new FormData();
-    formdata.append("img", img);
+  	var formdata=new FormData($("#imageForm")[0]);
     formdata.append("name", name);
     formdata.append("description", description);
     formdata.append("cat2_id", cat2_id);
