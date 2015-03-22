1c266689fc497ace670feb83641b29d08819090d
Yiqi <571738739@qq.com>
2015-03-22 11:26:00 +0800

    ...

--- app/static/admin/js/index_1.js
+++ app/static/admin/js/index_1.js
@@ -809,7 +809,7 @@ function modifyCat2(t, cat1Id) {
         var name = $(temp[0]).text();
     }
     var url="/admin/level1/cat2/modify";
-    var data ="cat2_id="+cat2_id +"&name" + name;
+    var data ="cat2_id="+cat2_id +"&name=" + name;
   	$.ajax({
    		type: "POST",
    		url: url,
